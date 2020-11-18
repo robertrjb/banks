831404134204b37cc4ad0f84b1942d0bcb4d095b
ROBERT JOSE BEZERRA <robert.jbezerra@senacsp.edu.br>
2020-11-18 11:33:43 -0300

    Adicionar arquivos de projeto.

---
+++ BankTests/BankAccountTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bank;
+
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdateBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Robert", beginningBalance);
+            //act
+            account.Debit(debitAmount);
+
+            //assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, account, "Débito não ocorreu corretamente");
+        }
+    }
+}
